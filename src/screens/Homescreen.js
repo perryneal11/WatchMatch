807968b28a85cf6361cb2b69239f964467b001b8
Perry <perryneal11@gmail.com>
2022-01-25 22:21:39 -0500

    added dislikes to filter, still need to fix rerendering undoing filter

--- src/screens/Homescreen.js
+++ src/screens/Homescreen.js
@@ -1,33 +1,38 @@
 import React, {useEffect, useState} from 'react';
-import {View, StyleSheet, Text, useWindowDimensions, Image} from 'react-native';
+import {View, StyleSheet, Text, useWindowDimensions, Image, ActivityIndicator} from 'react-native';
 import Card from '../component/ShowCard/';
 import AnimatedStack from '../component/AnimatedStack/';
 import {Auth, DataStore} from 'aws-amplify'
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import Entypo from 'react-native-vector-icons/Entypo';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import TopRow from '../component/ButtonBars/topRow';
 import {User} from '../models'
 
 const HomeScreen = () => {
-  const [movieData, setMovieData] = React.useState()
-  const [filteredData, setFilteredData] = React.useState(movieData)
-  const [currentMovie, setCurrentMovie] = React.useState()
-  const [user, setUser] = React.useState({})
 
   testData = 
-[
-  {title: "movie",
-  backdropPath: "/pYziM5SEmptPW0LdNhWvjzR2zD1.jpg",
-  overview: "This is an overview",
-  imdbID: 'tt9850370'
-  },
-  {title: "movie2",
-  backdropPath: "/a7f2CN7sBmFJJu5uO9BvDgqOEAc.jpg",
-  overview: "This is an overview",
-  imdbID: 'tt9850380'
-  }
-]
+  [
+    {title: "movie",
+    backdropPath: "/pYziM5SEmptPW0LdNhWvjzR2zD1.jpg",
+    overview: "This is an overview",
+    imdbID: 'tt9850370'
+    },
+    {title: "movie2",
+    backdropPath: "/a7f2CN7sBmFJJu5uO9BvDgqOEAc.jpg",
+    overview: "This is an overview",
+    imdbID: 'tt9850380'
+    },
+    {title: "movie3",
+    backdropPath: "/3uwyXMZN93PRkShUxvLrufwVAc2.jpg",
+    overview: "This is an overview",
+    imdbID: 'tt9850360'
+    }
+  ]
+  const [movieData, setMovieData] = React.useState(testData)
+  const [filteredData, setFilteredData] = React.useState(movieData)
+  const [currentMovie, setCurrentMovie] = React.useState()
+  const [isLoading, setIsLoading] = useState(false)
+  const [user, setUser] = React.useState({})
+
+
 
   const fetchData = async () => {
    fetch("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&output_language=en&language=en", {
@@ -47,23 +52,29 @@ const HomeScreen = () => {
   })}
 
   const getCurrentUser = async ()=> {
-    const user = await Auth.currentAuthenticatedUser()
-    const dbUsers = await DataStore.query(User, u => u.awsID === user.attributes.sub)
+    const userVar = await Auth.currentAuthenticatedUser()
+    console.log("user in get current user" , userVar.attributes)
+    const dbUsers = await DataStore.query(User, u => u.awsID("eq", userVar.attributes.sub))
+    console.log("dbusers", dbUsers)
     const dbUser = dbUsers[0];
-    console.log("help", user)
+    console.log("dbuser", dbUser)
     return setUser(dbUser)
     }
 
-  const filterMovieData = async (movieData) => {
+  const filterMovieData = async () => {
     if (user){
+      console.log("user", user)
     const likedMovies = user.approvedContentIMDBID
-    if(likedMovies != null){
-      setFilteredData(movieData.filter(item => !user.approvedContentIMDBID.includes(item.imdbID)))
-      return setMovieData(filteredData)
+    console.log("liked movies ", likedMovies)
+    const dislikedMovies = user.unapprovedContentIMDBID
+    console.log("disliked movies ", dislikedMovies)
+    if(likedMovies != null || dislikedMovies != null){
+      console.log("if a  ")
+      console.log("filtered ", movieData.filter(item => !likedMovies.includes(item.imdbID) && !dislikedMovies.includes(item.imdbID)))
+      return setMovieData(movieData.filter(item => !likedMovies.includes(item.imdbID) && !dislikedMovies.includes(item.imdbID)))
     }
     else return 
   }
-  else console.log(user)
   } 
 
   const save = async (newIMDBID, approved) => { 
@@ -95,24 +106,31 @@ const HomeScreen = () => {
   } 
 
   useEffect(()=>{
+    setIsLoading(true)
     //fetchData()
     getCurrentUser()
-    setMovieData(testData)
     filterMovieData(movieData)
-  }, [])
+    setIsLoading(false)
+  },)
   
 
   return (
     <View style={styles.pageContainer}>
+         {isLoading? 
+         (
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <ActivityIndicator size="large" color="#5500dc" />
+          </View>
+        ) : <Text></Text>}
 
-      {movieData ? (
+      {movieData.length > 0 ? (
             <AnimatedStack
               data={movieData}
               renderItem = {({item}) => <Card movie={item}image={item.backdropPath} />}
               onSwipeLeft = {onSwipeLeft}
               onSwipeRight = {onSwipeRight}
               setCurrentMovie = {setCurrentMovie}>
-            </AnimatedStack>) : (<Text>No Movie Data</Text>)}
+            </AnimatedStack>) : (<Text stlye = {styles.error}>No Movie Data</Text>)}
             <TopRow screen = 'HOME'></TopRow>
     </View>
   );
@@ -125,20 +143,22 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#ededed"
   },
-  icons: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    padding: 10,
-    width: '100%'
-  },
-  button: {
-    width: 50,
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
+  error: {
+    flex: 6,
     backgroundColor: 'white',
-    padding: 10,
-    borderRadius: 50,
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 8,
+    },
+    shadowOpacity: 0.46,
+    shadowRadius: 11.14,
+
+    elevation: 17,
+  
   }
 });
 
