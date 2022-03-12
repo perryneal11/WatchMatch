423ae2dc5d1c8a6d2b06a3c35c4de6a9f23a285c
Perry <perryneal11@gmail.com>
2022-03-12 00:06:13 -0500

    android fix and andrdoi icons

--- android/app/src/main/java/com/watchmatch/MainApplication.java
+++ android/app/src/main/java/com/watchmatch/MainApplication.java
@@ -4,11 +4,14 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.facebook.react.bridge.JSIModulePackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
